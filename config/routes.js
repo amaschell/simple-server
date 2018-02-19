aea3555bba5cfb688bfcc659ee0525c714971404
Amadeus Schell <amadeus.schell17@gmail.com>
2018-02-19 20:54:08 +0100

    Added /posts and /post/:id to the app's routes.
    For the posts: Still using test data in a simple JSON file.

---
+++ config/routes.js
@@ -0,0 +1,21 @@
+module.exports = function(app) {
+
+    app.get('/', function(req, res){
+        res.send('This is the home page!');
+    });
+
+    app.get('/about', function (req, res) {
+        res.send('This is the about page!');
+    });
+
+    app.get('/posts', function (req, res) {
+        const allPosts = require('../test_data/posts.json');
+        res.send(allPosts);
+    });
+
+    app.get('/posts/:id', function (req, res) {
+        const allPosts = require('../test_data/posts.json');
+        const post = allPosts.find(p => p.url === req.params.id)
+        res.send(post);
+    });
+};
