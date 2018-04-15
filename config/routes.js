f11ab4a9ebe2803ca4a1c0a164562dd64dcef372
Amadeus Schell <amadeus.schell17@gmail.com>
2018-04-15 19:56:04 +0200

    Added 404 route handling.

--- config/routes.js
+++ config/routes.js
@@ -13,19 +13,22 @@ module.exports = function(app) {
 
     app.get('/about', usersController.allUsers);
 
-    app.get('/posts', function (req, res) {
+    app.get('/posts', function(req, res) {
         const allPosts = require('../test_data/posts.json');
         res.send(allPosts);
     });
 
-
-    app.get('/posts/:id', function (req, res) {
+    app.get('/posts/:id', function(req, res) {
         const allPosts = require('../test_data/posts.json');
         const post = allPosts.find(p => p.url === req.params.id);
         res.send(post);
     });
 
-    app.post('/send-contact-mail', function (req, res) {
+    app.post('/send-contact-mail', function(req, res) {
         res.status(200).send('Mail was sent!');
+    });
+
+    app.get('*', function(req, res){
+        res.status(404).send('This page does not exist!');
     });
 };
